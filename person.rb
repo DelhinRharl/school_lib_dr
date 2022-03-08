aeb3f0ddb82200ea357a412b4e50c53e08ead7d2
DelhinRharl <delhinrharl@gmail.com>
2022-03-08 12:56:25 +0100

    Add the classes and methods

---
+++ person.rb
@@ -0,0 +1,24 @@
+class Person
+  def initalize(_age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = int
+    @parent_permission = parent_permission
+  end
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    if of_age || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def of_age
+    of_age >= 18
+  end
+end
---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require './person'
+class Teacher < Person
+  def initalize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
