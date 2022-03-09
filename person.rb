a7ffad76ae9809e8cb42667e7e2ab1ed5554b9ac
DelhinRharl <delhinrharl@gmail.com>
2022-03-09 10:03:23 +0100

    Add decorator class

--- person.rb
+++ person.rb
@@ -1,8 +1,15 @@
-class Person
+class Namable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Person < Namable
   attr_accessor :name, :age
   attr_reader :id
 
   def initalize(_age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = int
@@ -19,10 +26,39 @@ class Person
     end
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def is_of_age
     is_of_age >= 18
   end
   # rubocop:enable Naming/PredicateName
+end
+
+class BaseDecorator < Namable
+  attr_accessor :namable
+
+  def initialize(namable)
+    super()
+    @namable = namable
+  end
+
+  def correct_name
+    @namable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @namable.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @namable.correct_name.strip if @namable.correct_name.length <= 10
+  end
 end
